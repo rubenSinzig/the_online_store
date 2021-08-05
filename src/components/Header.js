3b81587ff5f4dcfc7304a4c62e0f7ca84cd97028
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-05 14:56:21 +0200

    create images folder in public & add img for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="wrapper-header"></div>;
+  return (
+    <div className="wrapper-header">
+      <img
+        src={`${process.env.PUBLIC_URL}/images/header_iPhone.png`}
+        alt="blue iPhone"
+      />
+    </div>
+  );
 };
 
 export default Header;
