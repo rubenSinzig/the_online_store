2d5182e45f4221dc330591d14369c369d6205688
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-05 13:38:50 +0200

    Add Header.js & sass -> layouts-dir

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import { useState } from "react"; // <-- for searchbar
 import { Store, StoreContext } from "./context";
 import ProductList from "./components/ProductList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <StoreContext.Provider value={Store}>
+      <Header />
       <h1>Online Store</h1>
 
       <ProductList />
