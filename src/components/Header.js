3a481d0762827618039a705a549c696a6bde93f0
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-05 15:32:15 +0200

    header.scss -> add image & text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,46 @@
 import React from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Header = () => {
   return (
+    // main wrapper for header
     <div className="wrapper-header">
+      {/* iPhone image */}
       <img
+        className="header-img"
         src={`${process.env.PUBLIC_URL}/images/header_iPhone.png`}
         alt="blue iPhone"
       />
+
+      {/* wrapper for iPhone text */}
+      <div className="header-text">
+        {/* iPhone text */}
+        <h2>iPhone</h2>
+        <h4>Almost too fast to be true</h4>
+
+        {/* links to product iPhone */}
+        <Router>
+          <div className="header-product-link">
+            <Link to="/#">
+              <h6>
+                More Info
+                <span>
+                  <FiArrowUpRight />
+                </span>
+              </h6>
+            </Link>
+            <Link to="/#">
+              <h6>
+                BUY
+                <span>
+                  <FiArrowUpRight />
+                </span>
+              </h6>
+            </Link>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 };
