4ce09b4dc75ccc4e0f1d655e9913e8c9c344576d
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-13 13:05:59 +0200

    Home -> add product text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Home = () => {
   return (
     <div className="wrapper-home">
-      <div className="home-products product-macbook">
+      {/* MacBook */}
+      <div className="home-products">
         <div className="home-img">
           <img
             src={`${process.env.PUBLIC_URL}/images/home_macbook.png`}
             alt="Apple macBook"
           />
         </div>
-        <div className="home-info macbook-info"></div>
+        <div className="home-info">
+          <h2>MacBook</h2>
+          <h4>Which Mac is right for you?</h4>
+          <Link className="home-link" to="/products">
+            BUY
+            <span>
+              <FiArrowUpRight />
+            </span>
+          </Link>
+        </div>
       </div>
+      {/* product two */}
+
+      {/* product three */}
+
+      {/* product four */}
+
+      {/* product five */}
     </div>
   );
 };
