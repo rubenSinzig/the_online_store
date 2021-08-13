c1692ae5b6b603b0bf916813382b165c2b6a9125
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-13 11:51:49 +0200

    create home.scss in layouts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="wrapper-home">
+      <div className="home-product product-macbook">
+        <div className="macbook-img"></div>
+        <div className="macbook-info"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
