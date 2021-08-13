432d40b4b6ad879d92bae5e60109272c79905104
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-13 12:26:41 +0200

    Home -> add product image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <div className="wrapper-home">
-      <div className="home-product product-macbook">
-        <div className="macbook-img"></div>
-        <div className="macbook-info"></div>
+      <div className="home-products product-macbook">
+        <div className="home-img">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/home_macbook.png`}
+            alt="Apple macBook"
+          />
+        </div>
+        <div className="home-info macbook-info"></div>
       </div>
     </div>
   );
