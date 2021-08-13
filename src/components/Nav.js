d82d9e6683910d4212574418fefa9925a2958cdb
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-13 11:02:11 +0200

    Nav -> position, size, color & hover

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,28 +6,27 @@ import About from "./About";
 
 const Nav = () => {
   return (
-    <div className="wrapper-nav">
-      <Router>
-        <div className="nav-home">
-          <Link to="/">Home</Link>
-        </div>
+    <Router>
+      <div className="wrapper-nav">
+        <Link className="nav-link nav-home" to="/">
+          Home
+        </Link>
 
-        <div className="nav-products">
-          <Link to="/products">Products</Link>
-        </div>
+        <Link className="nav-link nav-products" to="/products">
+          Products
+        </Link>
 
-        <div className="nav-about">
-          <Link to="/about">About</Link>
-        </div>
+        <Link className="nav-link nav-about" to="/about">
+          About
+        </Link>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={ProductList} />
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/products" component={ProductList} />
-
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </Router>
-    </div>
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </Router>
   );
 };
 
